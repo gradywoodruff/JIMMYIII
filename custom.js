abbf2e07ec6a3f12476620e28357ace8eec980be
grady <gradywoodruff@mac.com>
2016-09-19 16:20:32 -0400

    og

---
+++ custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    if ("ontouchstart" in document)
+    $('body').addClass('touch');
+
+    setTimeout(function(){
+        $('body').removeClass('loading');
+    }, 3600);
+    setTimeout(function(){
+        $('body').addClass('loaded');
+    }, 3600);
+  
+    var colors = ['#A74449', '#BDB5A8', '#40353D', '#AA9576', '#685656'];
+    $('body').css({'background-color': '' + colors[Math.floor(Math.random() * colors.length)]});
+
+});
